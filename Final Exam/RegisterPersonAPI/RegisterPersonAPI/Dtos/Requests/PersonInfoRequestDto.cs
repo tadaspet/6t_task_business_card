66b1624a486fc1bdff2d143dfccc208955426480
tadaspet <petruitis.tadas@gmail.com>
2024-02-09 18:24:59 +0200

    updated name and Surname regex

--- Final Exam/RegisterPersonAPI/RegisterPersonAPI/Dtos/Requests/PersonInfoRequestDto.cs
+++ Final Exam/RegisterPersonAPI/RegisterPersonAPI/Dtos/Requests/PersonInfoRequestDto.cs
@@ -5,10 +5,10 @@ namespace RegisterPersonAPI.Dtos.Requests
     public class PersonInfoRequestDto
     {
         [Required]
-        [RegularExpression(@"^ [A - Za - z]{1, 30}$", ErrorMessage = "Name can't be longer than 30 characters and shorter than 1 character.")]
+        [RegularExpression(@"^[a-zA-Z\s]{1,30}$", ErrorMessage = "Name must be between 1-30 characters long and contain only letters.")]
         public string Name { get; set; }
         [Required]
-        [RegularExpression(@"^ [A - Za - z]{1, 50}$", ErrorMessage = "Name can't be longer than 50 characters and shorter than 1 character.")]
+        [RegularExpression(@"^[a-zA-Z\s]{1,50}$", ErrorMessage = "Surname must be between 1-50 characters long and contain only letters.")]
         public string Surname { get; set; }
         [Required]
         [StringLength(20, MinimumLength = 10, ErrorMessage = "IdentityCode can't be longer than 20 characters and shorter than 10 characters.")]
